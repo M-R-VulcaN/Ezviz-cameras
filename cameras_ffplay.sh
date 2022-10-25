28313961a2a009dc93967d13d4a9d0570dbc2300
Maxim Gavrylyuk <83497212+1N0Face@users.noreply.github.com>
2022-10-25 11:16:48 +0300

    View streams using ffplay

---
+++ cameras_ffplay.sh
@@ -0,0 +1,5 @@
+#You should install ffmpeg to use ffplay (sudo apt install ffmpeg)
+ffplay rtsp://admin:UOXBGL@192.168.0.233:554/H.264 & 
+ffplay rtsp://admin:TGASLM@192.168.0.76:554/H.264 &
+ffplay rtsp://admin:VSYAJL@192.168.0.79:554/H.264 &
+ffplay rtsp://admin:IGXJVS@192.168.0.55:554/H.264
